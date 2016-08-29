970b061be7e02d178ce668f2f16b109024b2ae04
Hanif <h_ghazali@hotmail.com>
2016-08-30 01:21:57 +0800

    Import temp assets, made temp isometric grid system

---
+++ NM3216Empires/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject buildingPrefab;
+
+    Vector3 tileSizeInUnits = new Vector3(2.0f, 1.0f, 0.5f);
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Calculate ratios for simple grid snap
+            float xx = Mathf.Round(mousePos.y / tileSizeInUnits.y - mousePos.x / tileSizeInUnits.x);
+            float yy = Mathf.Round(mousePos.y / tileSizeInUnits.y + mousePos.x / tileSizeInUnits.x);
+
+            // Calculate grid aligned position from current position
+            //Vector3 position;
+            float x = (yy - xx) * 0.5f * tileSizeInUnits.x;
+            float y = (yy + xx) * 0.5f * tileSizeInUnits.y;
+            //float z = 1.0f * position.y - 0.1f * position.x;
+            //Debug.Log("X" + Mathf.Round(mousePos.x)*0.5f + "Y" + Mathf.Round(mousePos.y) * 0.5f);
+            //Vector2 translatedMousePos = new Vector2(Mathf.Round(mousePos.x) * 0.5f, Mathf.Round(mousePos.y) * 0.5f + 0.25f);
+            //find the nearest grid co-ord
+            //instantiate building
+            Instantiate(buildingPrefab, new Vector2(x, y+.25f),this.transform.rotation);
+
+        }
+	
+	}
+}
---
+++ NM3216Empires/Assets/Scripts/GridScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+[ExecuteInEditMode]
+public class GridScript : MonoBehaviour {
+
+    public GameObject gridNode; // Refering to the gird object prefab that we created
+
+    int gridHeight = 10; //This is how tall your grid will be
+    int gridWidth = 20; //This is how wide your grid will be
+
+    //void Update() // This is called every frame of the game
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space)) //Cheaking if the player pressed space
+    //    {
+    //        gridGen(); //Calling the gridGen function (below)
+    //    }
+    //}
+
+    void Start()  //If you want you can call this function at anytime with gridGen();
+    {
+        for (int vert = 0; vert < gridHeight; vert++) //adding 1 to vert everytime it is called
+        {
+            for (int hor = 0; hor < gridWidth; hor += 2) //adding 2 to hor everytime it is called
+            {
+                //Creating the prefab at the right x and y position:
+                GameObject gridObject = Instantiate(gridNode, new Vector3(hor, vert, 0), Quaternion.identity) as GameObject;
+                //Naming that object appropriatly:
+                gridObject.name = "X:" + gridObject.transform.position.x + "Y:" + gridObject.transform.position.y;
+                //EditorApplication.SaveScene();
+                //EditorSceneManager.SaveOpenScenes();
+            }
+        }
+    }
+}
