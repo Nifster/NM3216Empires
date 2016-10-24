5c9d2751cd43a63b36f9a32d314a1249f6b84b16
nifster <h_ghazali@hotmail.com>
2016-10-24 18:26:35 +0800

    Main Menu

---
+++ NM3216Empires/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject creditsPanel;
+	// Use this for initialization
+	void Start () {
+        creditsPanel.SetActive(false);
+	}
+	
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Platform");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+    }
+    
+    public void BackFromCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
