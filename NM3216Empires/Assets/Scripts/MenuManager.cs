b3c2107afadf1cc6224f3c942927f26d598eb7d0
Hanif <h_ghazali@hotmail.com>
2016-11-06 23:29:59 +0800

    Added credits and instructions, touched up main menu

--- NM3216Empires/Assets/Scripts/MenuManager.cs
+++ NM3216Empires/Assets/Scripts/MenuManager.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
+using System.Collections.Generic;
+using UnityEngine.UI;
 public class MenuManager : MonoBehaviour {
 
     public GameObject creditsPanel;
+    public GameObject instructionsPanel;
+    public List<Sprite> instructionsPanelSprites;
+    public GameObject nextInstructionButton;
+    int instructionsIndex;
+
 	// Use this for initialization
 	void Start () {
         creditsPanel.SetActive(false);
@@ -28,5 +34,34 @@ public class MenuManager : MonoBehaviour {
     public void BackFromCredits()
     {
         creditsPanel.SetActive(false);
+    }
+
+    public void InstructionsButton()
+    {
+        //activate instructionsPanel;
+        instructionsPanel.SetActive(true);
+    }
+
+    public void NextInstructionsButton()
+    {
+        if(instructionsIndex < instructionsPanelSprites.Count-1)
+        {
+            instructionsIndex++;
+            instructionsPanel.GetComponent<Image>().sprite = instructionsPanelSprites[instructionsIndex];
+            if(instructionsIndex >= 7)
+            {
+                //move the button to the new position
+                nextInstructionButton.GetComponent<RectTransform>().localPosition = new Vector3(276, -173, 0);
+            }
+        }
+        else
+        {
+            instructionsIndex = 0;
+            instructionsPanel.GetComponent<Image>().sprite = instructionsPanelSprites[instructionsIndex];
+            //move the button to the new position
+            nextInstructionButton.GetComponent<RectTransform>().localPosition = new Vector3(276, -246, 0);
+            instructionsPanel.SetActive(false);
+        }
+        
     }
 }
